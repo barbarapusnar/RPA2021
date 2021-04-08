878ef9c8055b2747d009bdccfcee8721625ae89e
Barbara Pušnar <barbara.pusnar@scng.si>
2021-04-08 06:37:27 +0200

    Razredi za vajo

---
+++ VajaCodeFirst/VajaCodeFirst/Models/Račun.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VajaCodeFirst.Models
+{
+    public class Račun
+    {
+        public int Id { get; set; }
+        public int IdKupca { get; set; }
+        public decimal Vrednost { get; set; }
+        public DateTime DatumIzdaje { get; set; }
+        
+        public Kupec Stranka { get; set; }
+    }
+}
