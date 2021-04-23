8ccf3e2e6b3adc24a2d901a0a58f67625df2e063
Barbara Pušnar <barbara.pusnar@scng.si>
2021-04-23 08:30:47 +0200

    novo

---
+++ TrgovinaWebAPI/TrgovinaWebAPI/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrgovinaWebAPI.Models
+{
+    public class Product
+    {
+        [ScaffoldColumn(false)] //s tem posatne ID identity
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal ActualCost { get; set; }
+    }
+}
